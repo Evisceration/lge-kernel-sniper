7d79890f30e01e1a80a48c178b6d13e051fbbfcd
Alexander Martinz <eviscerationls@gmail.com>
2013-08-01 17:14:37 +0200

    CPU, GPU Overclock, SIO scheduler, smartassv2 gov added
    Change-Id: I915b1ced609d2dcf62e3b932a3ef925ecb88912e

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -2,10 +2,10 @@
  * for MPU and CORE voltages
  */
 
-#define OMAP3630_VDD_MPU_OPP50_UV               1012500
-#define OMAP3630_VDD_MPU_OPP100_UV              1200000
+#define OMAP3630_VDD_MPU_OPP50_UV               980000
+#define OMAP3630_VDD_MPU_OPP100_UV              1162500
 #define OMAP3630_VDD_MPU_OPP120_UV              1325000
-#define OMAP3630_VDD_MPU_OPP1G_UV               1375000
+#define OMAP3630_VDD_MPU_OPP1G_UV               1425000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
 #define OMAP3630_VDD_CORE_OPP100_UV             1200000
