7ce442233b890ceaa9e40ce1562f6e87a0223203
Alexander Martinz <eviscerationls@gmail.com>
2013-09-09 18:24:22 +0200

    Change voltages and add 400mHz Frequency
    Change-Id: Icedf9f0f6ddf794230d23b94a11237a58f478330

--- arch/arm/mach-omap2/omap3_voltages.h
+++ arch/arm/mach-omap2/omap3_voltages.h
@@ -3,11 +3,11 @@
  */
 
 #define OMAP3630_VDD_MPU_OPP50_UV               1025000
+#define OMAP3630_VDD_MPU_OPP75_UV		1075000
 #define OMAP3630_VDD_MPU_OPP100_UV              1175000
-#define OMAP3630_VDD_MPU_OPP120_UV              1300000
-#define OMAP3630_VDD_MPU_OPP1G_UV               1400000
-#define OMAP3630_VDD_MPU_OPP2G_UV               1500000
-/*#define OMAP3630_VDD_MPU_OPP3G_UV               1550000*/
+#define OMAP3630_VDD_MPU_OPP120_UV              1250000
+#define OMAP3630_VDD_MPU_OPP1G_UV               1325000
+#define OMAP3630_VDD_MPU_OPP2G_UV               1400000
 
 #define OMAP3630_VDD_CORE_OPP50_UV              1000000
 #define OMAP3630_VDD_CORE_OPP100_UV             1200000
